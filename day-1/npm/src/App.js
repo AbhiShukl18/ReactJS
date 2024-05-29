1a3bfef038d474832c420894385faa9c271c818e
AbhiShukl18 <164164042+AbhiShukl18@users.noreply.github.com>
2024-05-29 09:05:06 +0530

    updates

--- day-1/npm/src/App.js
+++ day-1/npm/src/App.js
@@ -8,6 +8,8 @@ import Mens from './Component/Mens';
 import Profile from './Component/Profile';
 import UseState from './usestate 25may/UseState';
 import NotFound from './notfound';
+import UseEffect from './usestate 25may/UseEffect';
+import UseEffect2 from './usestate 25may/UseEffect2';
 
 
 
@@ -15,13 +17,16 @@ function App() {
   return (
     <div className="App">
       <Routes>
+      <Route path="*" element={<NotFound/>}/>
         <Route path= "/" element={<Home />} />
         <Route path= "/login" element={<Login />} />
         <Route path= "/register" element={<Register />} />
         <Route path= "/mens" element={<Mens />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/use-State" element= {< UseState />}/>
-        <Route path="*" element={<NotFound/>}/>
+        <Route path="/use-effect" element={< UseEffect/>} />
+        <Route path="/use-effect2" element={< UseEffect2/>} />
+
         </Routes>
         </div>  
   );
