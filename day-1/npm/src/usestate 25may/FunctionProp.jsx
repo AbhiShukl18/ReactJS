4cac0c6a4fa78774eaa3cd5758a7b95562e459e2
AbhiShukl18 <164164042+AbhiShukl18@users.noreply.github.com>
2024-06-05 22:34:08 +0530

    updates

--- day-1/npm/src/usestate 25may/FunctionProp.jsx
+++ day-1/npm/src/usestate 25may/FunctionProp.jsx
@@ -1,53 +1,53 @@
+// function FunctionProp(){
+
+
+//     function HandleClick(){
+
+//         alert("CLICKED")
+//     }
+
+//     return (
+
+//         <div>
+//             <h1>FunctionProp</h1>
+//             {/* function keyword can not be use in component */}
+//             <ChildrenComponent buttonValue="Click Me" HandleClick={HandleClick}></ChildrenComponent>   
+//             </div>
+//     );
+// }
+
+
+// function ChildrenComponent({buttonValue, HandleClick}){
+
+//     return <button onClick={HandleClick}>{buttonValue}</button>
+// }
+
+import { useState } from "react";
+
+
 function FunctionProp(){
 
+const[counter, setcounter]=useState(1);
 
-    function HandleClick(){
+function Increment(){
 
-        alert("CLICKED")
-    }
+    setcounter(counter+1);
 
+}
     return (
-
-        <div>
-            <h1>FunctionProp</h1>
-            {/* function keyword can not be use in component */}
-            <ChildrenComponent buttonValue="Click Me" HandleClick={HandleClick}></ChildrenComponent>   
-            </div>
+    <div>
+        <h1>FunctionProp</h1>
+        <h1>Counter:{counter} </h1>
+        <ChildrenComponent Increment={Increment}></ChildrenComponent>
+    </div>
     );
 }
 
 
-function ChildrenComponent({buttonValue, HandleClick}){
+function ChildrenComponent({Increment}){
 
-    return <button onClick={HandleClick}>{buttonValue}</button>
+    return(
+        <button onClick={Increment}>CLICK FOR + </button>
+    );
 }
-
-// import { useState } from "react";
-
-
-// function FunctionProp(){
-
-// const[counter, setcounter]=useState(1);
-
-// function Increment(){
-
-//     setcounter(counter+1);
-
-// }
-//     return (
-//     <div>
-//         <h1>FunctionProp</h1>
-//         <h1>Counter:{counter} </h1>
-//         <ChildrenComponent Increment={Increment}></ChildrenComponent>
-//     </div>
-//     );
-// }
-
-
-// function ChildrenComponent({Increment}){
-
-//     return(
-//         <button onClick={Increment}>CLICK FOR + </button>
-//     );
-// }
 export default FunctionProp;
