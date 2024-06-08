94c8ee6a1e65ed2e044770a9ca9cd33a8f3870f6
AbhiShukl18 <164164042+AbhiShukl18@users.noreply.github.com>
2024-06-08 22:54:09 +0530

    update

--- day-1/npm/src/Component/Login.jsx
+++ day-1/npm/src/Component/Login.jsx
@@ -1,5 +1,54 @@
-function Login(){
-    return <div>Login Kar lo yar...</div>;
+import React, { useState } from 'react'
+import {toast} from "react-hot-toast"
+import { useNavigate } from 'react-router-dom';
+
+const Login = () => {
+
+    const router= useNavigate();
+    const[userData, setUserData]= useState({email: "", password: ""});
+    function handlechange(event){
+
+        // console.log(event.target.value, event.target.name);
+        setUserData({ ...userData, [event.target.name] : [event.target.value]});
+
     }
-    
-    export default Login;
+
+   async function handleSubmit(event){
+        event.preventDefault();
+
+        try{
+            if(userData.email && userData.password){
+                // toast.success("Registration Successfull. Go for Login")
+                const response= {data: {success:true, message: "LOGIN Successfull."},}; 
+                if(response.data.success){
+                    setUserData({email: "", password: ""});
+                    
+                    toast.success(response.data.message);
+                    router("/")
+                }
+            }
+            else{
+                toast.error("All fields are mandatory")
+            }
+        }
+
+        catch(error){
+
+
+        }
+    }
+  return (
+    <div>
+      <form action="" onSubmit={handleSubmit}>
+        <h1>LOGIN</h1>
+        <label htmlFor="">Email: </label><br />
+        <input type="email" onChange={handlechange} name="email" id="" value={userData.email}/> <br />
+        <label htmlFor="">Password: </label><br />
+        <input type="password" onChange={handlechange} name="password" id="" value={userData.password}/> <br />
+        <button>Click to LOGIN</button>
+      </form>
+    </div>
+  )
+}
+
+export default Login
--- day-1/npm/src/Component/Register.jsx
+++ day-1/npm/src/Component/Register.jsx
@@ -1,5 +1,56 @@
-function Register(){
-    return <div> Register to karo pehle.......</div>;
+import React, { useState } from 'react'
+import {toast} from "react-hot-toast"
+import { useNavigate } from 'react-router-dom';
+
+const Register = () => {
+
+    const router= useNavigate();
+    const[userData, setUserData]= useState({name: "", email: "", password: ""});
+    function handlechange(event){
+
+        // console.log(event.target.value, event.target.name);
+        setUserData({ ...userData, [event.target.name] : [event.target.value]});
+
     }
-    
-    export default Register;
+
+   async function handleSubmit(event){
+        event.preventDefault();
+
+        try{
+            if(userData.name && userData.email && userData.password){
+                // toast.success("Registration Successfull. Go for Login")
+                const response= {data: {success:true, message: "Registration Successfull. Please Login now."},}; 
+                if(response.data.success){
+                    setUserData({name:"", email: "", password: ""});
+                    
+                    toast.success(response.data.message);
+                    router("/login")
+                }
+            }
+            else{
+                toast.error("All fields are mandatory")
+            }
+        }
+
+        catch(error){
+
+
+        }
+    }
+  return (
+    <div>
+      <form action="" onSubmit={handleSubmit}>
+        <h1>REGISTER</h1>
+        <label htmlFor="">Name: </label><br />
+        <input type="text" onChange={handlechange} name="name" id="" value={userData.name} /> <br />
+        <label htmlFor="">Email: </label><br />
+        <input type="email" onChange={handlechange} name="email" id="" value={userData.email}/> <br />
+        <label htmlFor="">Password: </label><br />
+        <input type="password" onChange={handlechange} name="password" id="" value={userData.password}/> <br />
+        <button>Click to Register</button>
+      </form>
+    </div>
+  )
+}
+
+export default Register
