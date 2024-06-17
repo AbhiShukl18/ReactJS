e4745b7ad143e6e764381d679e03a9c173d48c22
AbhiShukl18 <164164042+AbhiShukl18@users.noreply.github.com>
2024-06-17 21:56:30 +0530

    update

--- day-1/npm/src/usestate 25may/Reducer.jsx
+++ day-1/npm/src/usestate 25may/Reducer.jsx
@@ -1,50 +1,54 @@
-import React, { useReducer } from 'react'
+//useReducer(function, initialstate)
 
+import React, { useReducer } from "react";
 
-function reducer(state, action){
-
-    switch(action.type){
-
-        case "INCREMENT":
-        return {...state, counter:state.counter+1 };
-        case "DECREMENT":
-            return {...state, counter:state.counter-1 };;
-        case "RESET":
-            return {...state, counter:0};
-        default :   
-        return state;
-    }
-
+function reducer(state, action) {               //action-it has an object which has 'type' key, state= {counter:0}
+  switch (action.type) {
+    case "INCREMENT":
+      return { ...state, counter: state.counter + 1 };
+    case "DECREMENT":
+      return { ...state, counter: state.counter - 1 };
+    case "RESET":
+      return { ...state, counter: 0 };
+    default:
+      return state;
+  }
 }
-const initialState={counter: 0};
-
+const initialState = { counter: 0 };
 
 const Reducer = () => {
-    const[state, dispatch]=useReducer(reducer, initialState)
-    function Increment(){
-        dispatch({type: "INCREMENT"});
-    }
+  const [state, dispatch] = useReducer(reducer, initialState);   //dispatch is function
+  function Increment() {
+    dispatch({ type: "INCREMENT" });
+  }
 
-    function Decrement(){
-        dispatch({type: "DECREMENT"});
-
-
-    }
-    function Reset(){
-
-        dispatch({type: "RESET"});
-
-    }
+  function Decrement() {
+    dispatch({ type: "DECREMENT" });
+  }
+  function Reset() {
+    dispatch({ type: "RESET" });
+  }
 
   return (
     <div>
       <h1>Counter: {state.counter}</h1>
-     <h2> <button onClick={Increment}>PLUS</button> 
-      <button onClick={Decrement}>MINUS</button>
-      <button onClick={Reset}>RESET</button></h2>
-
+      <h2>
+        {" "}
+        <button onClick={Increment}>PLUS</button>
+        <button onClick={Decrement}>MINUS</button>
+        <button onClick={Reset}>RESET</button>
+      </h2>
     </div>
-  )
-}
+  );
+};
 
-export default Reducer
+export default Reducer;
+
+
+
+// Execution(In case of PLUS):-
+// 1. Line no 37
+// 2. Line no 21,22
+// 3. Line no 20
+// 4. Line no 5
+// 5. Line no 7
