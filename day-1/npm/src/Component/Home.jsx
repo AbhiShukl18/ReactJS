adb89fc69d3e3db0e0c59de1b67fea0592ce0385
AbhiShukl18 <164164042+AbhiShukl18@users.noreply.github.com>
2024-06-19 22:56:33 +0530

    update

--- day-1/npm/src/Component/Home.jsx
+++ day-1/npm/src/Component/Home.jsx
@@ -1,33 +1,59 @@
 import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import { authcontext } from "../Context/authcontext";
+import {themeContext} from "../Context/themecontext";
 
 function Home(){
-    const{state}= useContext(authcontext)
+    const{state}= useContext(authcontext);  
+    const{themeState, dispatch}=useContext(themeContext);
     const router=useNavigate();
-    return <div> 
-        <h1>Home- Hi!! {state?.user?.name} </h1>
-        <p><button onClick={()=> router("/Login")}>Go to Login page</button></p>
-        <p><button onClick={()=> router("/Register")}>Go to Register page</button></p>
-        <p><button onClick={()=> router("/Mens")}>Go to Mens page</button></p>
-        <p><button onClick={()=> router("/Profile")}>Go to Profile page</button></p>
-        <p><button onClick={()=> router("/Use-State")}>Go to UseState page</button></p>
-        <p><button onClick={()=> router("/Use-Effect")}>Go to UseEffect page</button></p>
-        <p><button onClick={()=> router("/Use-Effect2")}>Go to UseEffect2 page</button></p>
-        <p><button onClick={()=> router("/Use-Effect3")}>Go to UseEffect3 page</button></p>
-        <p><button onClick={()=> router("/use-navigate")}>Go to Navigate page</button></p>
-        <p><button onClick={()=> router("/mapping")}>Go to Mapping page</button></p>
-        <p><button onClick={()=> router("/all-products")}>Go to All products page</button></p>
-        <p><button onClick={()=> router("/function-props")}>Go to Function-props page</button></p>
-        <p><button onClick={()=> router("/styled-component")}>Go to styled-components page</button></p>
-        <p><button onClick={()=> router("/inline-style")}>Go to inline-styling page</button></p>
-        <p><button onClick={()=> router("/todo")}>Go to Todo page</button></p>
-        <p><button onClick={()=> router("/reducer")}>Go to Reducer page</button></p>
+    return(
 
-
-
-    </div>;
-   
+     <div style=
+     { 
+        {
+            backgroundColor: themeState.theme==="light"? "white": "#292929",
+            color: themeState.theme==="light"? "#292929": "white",
+          
+            }}> 
+            
+            <h1>Home- Hi!! {state?.user?.name} </h1>
+            <button style={{marginLeft: "90%",
+                fontSize:"14px",
+                width: "150px",
+                height: "40px",
+                backgroundColor: themeState.theme==="light"? "wheat": "white",
+                color: themeState.theme==="light"? "black": "black",
+                cursor:"pointer",
+            }}
+        onClick={() =>
+          dispatch({ type: themeState?.theme === "light" ? "DARK" : "LIGHT" })
+        }
+      >
+        Switch to {themeState?.theme === "dark" ? "light" : "dark"} Mode
+      </button>
+            <p><button onClick={()=> router("/Login")}>Go to Login page</button></p>
+            <p><button onClick={()=> router("/Register")}>Go to Register page</button></p>
+            <p><button onClick={()=> router("/Mens")}>Go to Mens page</button></p>
+            <p><button onClick={()=> router("/Profile")}>Go to Profile page</button></p>
+            <p><button onClick={()=> router("/Use-State")}>Go to UseState page</button></p>
+            <p><button onClick={()=> router("/Use-Effect")}>Go to UseEffect page</button></p>
+            <p><button onClick={()=> router("/Use-Effect2")}>Go to UseEffect2 page</button></p>
+            <p><button onClick={()=> router("/Use-Effect3")}>Go to UseEffect3 page</button></p>
+            <p><button onClick={()=> router("/use-navigate")}>Go to Navigate page</button></p>
+            <p><button onClick={()=> router("/mapping")}>Go to Mapping page</button></p>
+            <p><button onClick={()=> router("/all-products")}>Go to All products page</button></p>
+            <p><button onClick={()=> router("/function-props")}>Go to Function-props page</button></p>
+            <p><button onClick={()=> router("/styled-component")}>Go to styled-components page</button></p>
+            <p><button onClick={()=> router("/inline-style")}>Go to inline-styling page</button></p>
+            <p><button onClick={()=> router("/todo")}>Go to Todo page</button></p>
+            <p><button onClick={()=> router("/reducer")}>Go to Reducer page</button></p>
+         
+      <br /><br /> <br /><br /><br /><br /><br /> <br /><br /><br /><br /><br /><br />
+          
+    
+    </div>
+   );
 
     }
     
--- day-1/npm/src/index.js
+++ day-1/npm/src/index.js
@@ -6,10 +6,14 @@ import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from "react-router-dom"
 import {Toaster} from "react-hot-toast"
 import MyContextProvider from './Context/authcontext';
+import ThemeContextProvider from './Context/themecontext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <React.StrictMode>
+  <ThemeContextProvider>
   <MyContextProvider>
+    
   <BrowserRouter> 
 <Toaster
   position="top-center"
@@ -39,8 +43,12 @@ root.render(
     <App />
     </BrowserRouter>
     </MyContextProvider>
+    </ThemeContextProvider>
+    </React.StrictMode>
+
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
